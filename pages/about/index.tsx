e85dcc981546b956a520f77fbe1300fcd6d7d7c4
Daffawijaya <daffayanwijaya@gmail.com>
2022-12-27 11:54:12 +0700

    a

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -4,8 +4,201 @@ import Sidebar from '../../components/Sidebar'
 import Link from 'next/link';
 import Contact from '../../components/Contact';
 import { motion } from 'framer-motion';
-import { techstack, skills, experience } from '../../data/aboutData';
 import Image from 'next/image';
+import { SiRedux, SiFramer,SiPython, SiCsharp, SiArduino, SiAdobephotoshop, SiAdobepremierepro, SiBlender, SiFigma, SiMicrosoftoffice, SiTypescript, SiJavascript, SiReact, SiHtml5, SiCss3, SiTailwindcss, SiChakraui, SiBootstrap, SiNextdotjs } from "react-icons/si";
+import { HiPencil } from "react-icons/hi";
+
+export const techstack = [
+    {
+        title: "Programming Language",
+        item: [
+            {
+                name: "Javascript",
+                icon: <SiJavascript className='h-10 w-10' />,
+                color: "hover:text-[#EAD41C]",
+            },
+            {
+                name: "Typescript",
+                icon: <SiTypescript className='h-10 w-10' />,
+                color: "hover:text-[#2F72BC]",
+            },
+        ]
+    },
+    {
+        title: "Frontend Development",
+        item: [
+            {
+                name: "HTML",
+                icon: <SiHtml5 className='h-10 w-10' />,
+                color: "hover:text-[#D84924]",
+            },
+            {
+                name: "CSS",
+                icon: <SiCss3 className='h-10 w-10' />,
+                color: "hover:text-[#2449D8]",
+            },
+            {
+                name: "Tailwind",
+                icon: <SiTailwindcss className='h-10 w-10' />,
+                color: "hover:text-[#39A7A6]",
+            },
+            {
+                name: "Chakra UI",
+                icon: <SiChakraui className='h-10 w-10' />,
+                color: "hover:text-[#6BC0C5]",
+            },
+            {
+                name: "Bootstrap",
+                icon: <SiBootstrap className='h-10 w-10' />,
+                color: "hover:text-[#523A76]",
+            },
+            {
+                name: "Framer",
+                icon: <SiFramer className='h-10 w-10' />,
+                color: "hover:text-[#DC01C2]",
+            },
+            {
+                name: "Redux",
+                icon: <SiRedux className='h-10 w-10' />,
+                color: "hover:text-[#7046B3]",
+            },
+        ]
+    },
+    {
+        title: "Framework",
+        item: [
+            {
+                name: "React JS",
+                icon: <SiReact className='h-10 w-10' />,
+                color: "hover:text-[#00CDF2]",
+            },
+            {
+                name: "Next JS",
+                icon: <SiNextdotjs className='h-10 w-10' />,
+                color: "hover:text-white",
+            },
+        ]
+    },
+]
+
+export const skills = [
+    {
+        name: "Python",
+        icon: <SiPython className='h-10 w-10' />,
+        color: "hover:text-[#F2B92F]",
+    },
+    {
+        name: "C",
+        icon: <SiCsharp className='h-10 w-10' />,
+        color: "hover:text-[#045494]",
+    },
+    {
+        name: "Arduino",
+        icon: <SiArduino className='h-10 w-10' />,
+        color: "hover:text-[#009297]",
+    },
+    {
+        name: "Coreldraw",
+        icon: <HiPencil className='h-10 w-10' />,
+        color: "hover:text-[#5AA141]",
+    },
+    {
+        name: "Photoshop",
+        icon: <SiAdobephotoshop className='h-10 w-10' />,
+        color: "hover:text-[#001C33]",
+    },
+    {
+        name: "Premiere",
+        icon: <SiAdobepremierepro className='h-10 w-10' />,
+        color: "hover:text-[#280031]",
+    },
+    {
+        name: "Blender",
+        icon: <SiBlender className='h-10 w-10' />,
+        color: "hover:text-[#DE7000]",
+    },
+    {
+        name: "Figma",
+        icon: <SiFigma className='h-10 w-10' />,
+        color: "hover:text-[#09C47C]",
+    },
+    {
+        name: "Microsoft",
+        icon: <SiMicrosoftoffice className='h-10 w-10' />,
+        color: "hover:text-[#DF3A01]",
+    },
+]
+
+export const experience = [
+    {
+        title: "Internship",
+        item: [
+            {
+                name: "MBKM × Social Economic Accelerator Lab",
+                image: "bg-[url(/experience/seal2.png)]",
+                url: "https://seal.or.id/",
+                year: "August 2022 - December 2022"
+            },
+        ]
+    },
+    {
+        title: "Organization",
+        item: [
+            {
+                name: "Ikasti",
+                image: "bg-[url(/experience/ikasto.png)]",
+                url: "https://www.instagram.com/ikastiofficial/",
+                year: "August 2020 - Present"
+            },
+            {
+                name: "OSIS",
+                image: "bg-[url(/experience/osis.jpg)]",
+                url: "https://www.instagram.com/ospk_smagatgr/",
+                year: "July 2017 - July 2018"
+            },
+        ]
+    },
+    {
+        title: "Education",
+        item: [
+            {
+                name: "Muhammadiyah Malang University",
+                image: "bg-[url(/experience/umm.png)]",
+                url: "https://www.umm.ac.id/",
+                year: "August 2019 - Present"
+            },
+            {
+                name: "SMA Negeri 3 Unggulan Tenggarong",
+                image: "bg-[url(/experience/smaga.png)]",
+                url: "http://www.sman3tenggarong.sch.id/",
+                year: "June 2016 - June 2019"
+            },
+        ]
+    },
+    {
+        title: "Certification",
+        item: [
+            {
+                name: "SEAL Internship Graduate",
+                image: "bg-[url(/experience/seal2.png)]",
+                url: "https://drive.google.com/file/d/1kA6ukvDQoVK4PAAmr2n_N-OeKnwLoxuw/view?usp=sharing",
+                year: "December 2022"
+            },
+            {
+                name: "AWS Academy Cloud Architecting",
+                image: "bg-[url(/experience/aws.jpg)]",
+                url: "https://www.credly.com/badges/1c7ecf10-0045-4431-9791-d43ec0e1ac0c/linked_in_profile",
+                year: "November 2022"
+            },
+            {
+                name: "IC3 Digital Literacy",
+                image: "bg-[url(/experience/IC3.png)]",
+                url: "https://drive.google.com/file/d/1FiavmAuL1vkFKUZi4xMWKIjpFSNKOimw/view",
+                year: "November 2019"
+            },
+        ]
+    },
+]
 
 const About = () => {
     return (
--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -4,7 +4,28 @@ import Sidebar from '../../components/Sidebar'
 import Link from 'next/link'
 import Contact from '../../components/Contact'
 import { motion } from 'framer-motion'
-import { projects } from '../../data/worksData'
+
+export const projects = [
+    {
+        name: "SIPP: Sistem Informasi Penataan dan Penaatan Kota Batu",
+        image: "bg-[url(/batu.png)]",
+        url: "https://sipp-dev.vercel.app",
+        year: "December 2022"
+    },
+
+    {
+        name: "UMM research network Re-engineering",
+        image: "bg-[url(/experience/umm.png)]",
+        url: "http://ummrn-daffacindy.vercel.app/",
+        year: "November 2022"
+    },
+    {
+        name: "SEAL: Simple Todo List Bounding Project",
+        image: "bg-[url(/experience/seal2.png)]",
+        url: "https://mini-project-todolist-seal-5.vercel.app",
+        year: "August 2022"
+    },
+]
 
 const Works = () => {
     return (
