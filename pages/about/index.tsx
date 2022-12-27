31703ded3206a537ea1128c9970a7d5a5a7a4dbb
Daffawijaya <daffayanwijaya@gmail.com>
2022-12-28 00:28:04 +0700

    test deploy

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -287,7 +287,7 @@ const About = () => {
                 </div>
 
                 {/* techstack */}
-                {/* <div className='ml-[15%] relative z-10 h-screen italic snap-start'>
+                <div className='ml-[15%] relative z-10 h-screen italic snap-start'>
                     <div className='flex lg:flex-row flex-col lg:items-center justify-center h-full w-full p-6 lg:space-x-14'>
                         <div className='flex flex-col lg:w-[50%]'>
                             <p className='text-black dark:text-white md:text-xl text-sm'>Professional</p>
@@ -311,7 +311,7 @@ const About = () => {
                         </div>
                     </div>
                     <h1 className='absolute lg:flex hidden text-b-2 text-5xl right-0 bottom-40 -rotate-90 font-bold'>TECHSTACK</h1>
-                </div> */}
+                </div>
 
                 {/*other skill */}
                 {/* <div className='ml-[15%] relative z-10 h-screen italic snap-start'>
