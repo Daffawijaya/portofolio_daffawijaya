f35e49daf461ceec806385d345c108bed0f17b0f
unknown <daffayanwijaya.com>
2026-07-05 19:20:19 +0800

    Update portfolio

---
+++ components/WorkList.tsx
@@ -0,0 +1,75 @@
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+
+interface WorkItem {
+  name: string
+  image: string
+  url: string
+  year: string
+  type?: string
+}
+
+interface WorkListProps {
+  worksData: WorkItem[]
+}
+
+export default function WorkList({ worksData }: WorkListProps) {
+  return (
+    <div className='pl-[15%] relative z-10 h-full w-full overflow-y-scroll scrollbar-hide'>
+      <div className='flex flex-col lg:ml-[16%] space-y-6 italic'>
+        {worksData.map((item, idx) => (
+          <motion.div
+            key={idx}
+            initial={{ top: -400, opacity: 0 }}
+            animate={{ top: 0, opacity: 100 }}
+            transition={{
+              delay: 0.8,
+              duration: 1,
+            }}
+            className='relative'
+          >
+            {idx % 2 === 0 ? (
+              <div className='ml-[20%] relative'>
+                <Link href={item.url}>
+                  <div
+                    className={`lg:h-64 h-40 flex items-center ${item.image} bg-cover bg-center overflow-hidden justify-start`}
+                  >
+                    <div className='absolute h-full w-full backdrop-brightness-[0.3] hover:backdrop-brightness-[0.5] backdrop-saturate-0 hover:backdrop-saturate-100 backdrop-contrast-[0.8] hover:backdrop-contrast-[1]'>
+                      <div className='lg:px-[20%] px-[5%] absolute text-transparent flex flex-col items-center justify-center h-full w-full hover:text-white pt-3 hover:pt-0 duration-300'>
+                        <p className='lg:text-3xl text-xl font-bold capitalize text-left text-white w-full'>
+                          {item.name}
+                        </p>
+                        <p className='text-left capitalize w-full lg:text-base text-xs'>
+                          {item.year}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ) : (
+              <div className='mr-[20%] relative'>
+                <Link href={item.url}>
+                  <div
+                    className={`lg:h-64 h-40 flex items-center ${item.image} bg-cover bg-center overflow-hidden justify-end`}
+                  >
+                    <div className='absolute h-full w-full backdrop-brightness-[0.3] hover:backdrop-brightness-[0.5] backdrop-saturate-0 hover:backdrop-saturate-100 backdrop-contrast-[0.8] hover:backdrop-contrast-[1]'>
+                      <div className='lg:px-[20%] px-[5%] absolute text-transparent flex flex-col items-center justify-center h-full w-full hover:text-white pt-3 hover:pt-0 duration-300'>
+                        <p className='font-bold lg:text-3xl text-xl capitalize text-right text-white w-full'>
+                          {item.name}
+                        </p>
+                        <p className='text-right capitalize w-full lg:text-base text-xs'>
+                          {item.year}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            )}
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  )
+}
--- data/worksData.tsx
+++ data/worksData.tsx
@@ -1,21 +1,26 @@
-export const projects = [
-    {
-        name: "SIPP: Sistem Informasi Penataan dan Penaatan Kota Batu",
-        image: "bg-[url(/batu.png)]",
-        url: "https://sipp-dev.vercel.app",
-        year: "December 2022"
-    },
+const worksData = [
+  {
+    name: "SIPP: Sistem Informasi Penataan dan Penaatan Kota Batu",
+    image: "bg-[url(/batu.png)]",
+    url: "https://sipp-dev.vercel.app",
+    year: "December 2022",
+    type: "frontend",
+  },
 
-    {
-        name: "UMM research network Re-engineering",
-        image: "bg-[url(/experience/umm.png)]",
-        url: "http://ummrn-daffacindy.vercel.app/",
-        year: "November 2022"
-    },
-    {
-        name: "SEAL: Simple Todo List Bounding Project",
-        image: "bg-[url(/experience/seal2.png)]",
-        url: "https://mini-project-todolist-seal-5.vercel.app",
-        year: "August 2022"
-    },
-]
+  {
+    name: "UMM research network Re-engineering",
+    image: "bg-[url(/experience/umm.png)]",
+    url: "http://ummrn-daffacindy.vercel.app/",
+    year: "November 2022",
+    type: "frontend",
+  },
+  {
+    name: "SEAL Bounding Project: Todo List app for student",
+    image: "bg-[url(/experience/seal2.png)]",
+    url: "https://mini-project-todolist-seal-5.vercel.app",
+    year: "August 2022",
+    type: "frontend",
+  },
+];
+
+export default worksData;
