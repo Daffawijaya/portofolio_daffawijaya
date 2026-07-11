8052c7a4842e2434cdd3112e8328a0a877880e12
unknown <daffayanwijaya.com>
2026-07-11 18:59:02 +0800

    add status

--- data/frontendData.tsx
+++ data/frontendData.tsx
@@ -2,7 +2,7 @@ const frontendData = [
   {
     name: "EtamHub: digital directory platform for UMKM in Kutai Kartanegara",
     image: "EtamHub.png",
-    url: "https://sipp-dev.vercel.app",
+    url: "https://etamhub.vercel.app/",
     year: "January 2026 - Present",
     type: "frontend",
   },
